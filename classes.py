8f1347998a9040f443d98b11e2f6950f0f3bb5cd
chickfilangel <43784681+chickfilangel@users.noreply.github.com>
2019-01-30 12:44:29 -0500

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,6 @@
+classes = [ 'english' , 'math', 'history', 'science', 'gym', 'health', 'advisory', 'production' ]
+
+for classes in classes:
+	print('You have been enrolled in ' + classes + '.' )
+
+
---
+++ numbers.py
@@ -0,0 +1,5 @@
+numbers = [ '3', '5', '9', '17', '123', '666', '1000', '4321']
+
+for numbers in numbers:
+	numbers = int(numbers)
+	print(numbers * 2)
